566420640aa2e9bc39725886fe429c936c2f9f9f
Gary Hughes <garyedwardhughes@gmail.com>
2020-03-30 21:16:19 +1100

    Start defining tests.

--- libutility_tests/read_file_tests.cpp
+++ libutility_tests/read_file_tests.cpp
@@ -1,2 +1,16 @@
 #include <catch.hpp>
+#include <libutility/read_file.hpp>
 
+using namespace utility;
+
+TEST_CASE( "read_file", "[read_file]" ) {
+
+    SECTION( "reading a non existent file returns a stream in an error state" ) {
+        read_file("/no_such_file", 
+            [&](std::istream& is)
+            {
+                REQUIRE(!is);
+            });    
+    }
+
+}
