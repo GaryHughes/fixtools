d9a0ff724b0550ca96f4b6eaefd06f60fa56cdca
Gary Hughes <garyedwardhughes@gmail.com>
2020-04-01 18:51:48 +1100

    Basic parsing and printing.

--- fixcat/main.cpp
+++ fixcat/main.cpp
@@ -1,10 +1,47 @@
 #include <fix8/f8includes.hpp>
+#include <boost/algorithm/string.hpp>
 #include "libschema/FIX50SP2_types.hpp"
 #include "libschema/FIX50SP2_router.hpp"
 #include "libschema/FIX50SP2_classes.hpp"
 #include "options.hpp"
+#include "libutility/read_file.hpp"
 
+using namespace utility;
 using namespace FIX8;
+
+const char* fix_message_prefix = "8=FIX";
+
+void pretty_print_line(const std::string& line)
+{
+    auto start_of_message = line.find(fix_message_prefix);
+
+    if (start_of_message == std::string::npos)
+    {
+        return;
+    }
+
+    auto message_text = line.substr(start_of_message);
+    boost::algorithm::trim(message_text);
+
+    // fix8 is stricter than we would like for this kind of activity
+    // TODO - add the ability to parse partial messages
+    auto checksum_pos = message_text.find_last_of('\x01' + "10=");
+
+    if (checksum_pos == std::string::npos)
+    {
+        return;
+    }
+
+    message_text = message_text.substr(0, checksum_pos + 6);
+
+    // TODO - improve the diagnostics in this function particularly when the checksum bit fails @ line 259
+    std::unique_ptr<Message> message(Message::factory(FIX8::FIX50SP2::ctx(), message_text, true, true));
+
+    if (message)
+    {
+        std::cout << *message << std::endl;
+    }
+}
 
 int main(int argc, const char** argv)
 {
@@ -18,7 +55,23 @@ int main(int argc, const char** argv)
             return 1;
         }
 
-        auto message = Message::factory(FIX8::FIX50SP2::ctx(), "", true, true);
+        for (const auto filename : options.input_files())
+        {
+            read_file(filename, [](std::istream& is)
+            {
+                for (;;)
+                {
+                    std::string line;
+                    
+                    if (!std::getline(is, line))
+                    {
+                        break;
+                    }
+
+                    pretty_print_line(line);
+                }
+            });
+        }
     }
     catch (std::exception& ex)
     {
--- libutility_tests/read_file_tests.cpp
+++ libutility_tests/read_file_tests.cpp
@@ -3,14 +3,93 @@
 
 using namespace utility;
 
-TEST_CASE( "read_file", "[read_file]" ) {
+TEST_CASE("read_file", "[read_file]") {
 
-    SECTION( "reading a non existent file returns a stream in an error state" ) {
+    SECTION("reading a non existent file returns a stream in an error state") {
         read_file("/no_such_file", 
             [&](std::istream& is)
             {
                 REQUIRE(!is);
             });    
+    }
+
+    SECTION("read an uncommpressed file") {
+        std::vector<std::string> lines;
+        read_file("uncompressed.txt", 
+            [&](std::istream& is) 
+            {
+                REQUIRE(is); 
+                for (;;)
+                {
+                    std::string line;
+                    if (!std::getline(is, line))
+                    {
+                        break;
+                    }
+                    lines.push_back(line);
+                }
+            });
+        REQUIRE(lines == std::vector<std::string>({ "bart", "nelson", "milhouse" }));
+    }
+
+    SECTION("read a gzip commpressed file") {
+        std::vector<std::string> lines;
+        read_file("compressed.txt.gz", 
+            [&](std::istream& is) 
+            {
+                REQUIRE(is); 
+                for (;;)
+                {
+                    std::string line;
+                    if (!std::getline(is, line))
+                    {
+                        break;
+                    }
+                    lines.push_back(line);
+                }
+            });
+        REQUIRE(lines == std::vector<std::string>({ "bart", "nelson", "milhouse" }));
+    }
+
+    SECTION("read a bzip commpressed file") {
+        std::vector<std::string> lines;
+        read_file("compressed.txt.bz2", 
+            [&](std::istream& is) 
+            {
+                REQUIRE(is); 
+                for (;;)
+                {
+                    std::string line;
+                    if (!std::getline(is, line))
+                    {
+                        break;
+                    }
+                    lines.push_back(line);
+                }
+            });
+        REQUIRE(lines == std::vector<std::string>({ "bart", "nelson", "milhouse" }));
+    }
+
+    SECTION("read a file with a gzip filename that is not actually compressed") {
+        read_file("uncompressed.txt.gz",
+            [&](std::istream& is) 
+                {
+                    REQUIRE(is); 
+                    std::string line;
+                    REQUIRE(!std::getline(is, line));
+                }
+            );
+    }
+
+    SECTION("read a file with a bzip filename that is not actually compressed") {
+        read_file("uncompressed.txt.bz2",
+            [&](std::istream& is) 
+                {
+                    REQUIRE(is); 
+                    std::string line;
+                    REQUIRE(!std::getline(is, line));
+                }
+            );
     }
 
 }
